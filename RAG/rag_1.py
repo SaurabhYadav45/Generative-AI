175aa66eafe19c51cd9c52b0d8b60a9603244500
SaurabhYadav45 <saurabhkry08@gmail.com>
2025-04-23 11:49:20 +0530

    Rag_1

--- RAG/rag_1.py
+++ RAG/rag_1.py
@@ -5,6 +5,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
+from openai import OpenAI
 
 load_dotenv()
 
@@ -31,14 +32,14 @@ embedder = OpenAIEmbeddings(
   api_key=my_api_key
   )
 
-vector_store = QdrantVectorStore.from_documents(
-    documents=[],
-    embedding = embedder,
-    url="http://localhost:6333",
-    collection_name="my_documents",
-)
+# vector_store = QdrantVectorStore.from_documents(
+#     documents=[],
+#     embedding = embedder,
+#     url="http://localhost:6333",
+#     collection_name="my_documents",
+# )
 
-vector_store.add_documents(documents=split_docs)
+# vector_store.add_documents(documents=split_docs)
 print("Injection Done")
 
 retriver = QdrantVectorStore.from_existing_collection(
@@ -48,8 +49,41 @@ retriver = QdrantVectorStore.from_existing_collection(
 )
 
 
-search_result = retriver.similarity_search(
+relevant_chunk = retriver.similarity_search(
     query="What is FS Module?"
 )
 
-print("Relevant Chunks", search_result)
+# print("Relevant Chunks", relevant_chunk)
+
+formatted = []
+for doc in relevant_chunk:
+    snippet = f" [Page {doc.metadata.get('page')}] \n {doc.page_content}"
+    formatted.append(snippet)
+
+# print("Formatted :", formatted)
+context = "\n\n".join(formatted)
+# print("Context:", context)
+
+client = OpenAI(api_key=my_api_key)
+
+system_prompt = f"""
+You're helpful AI assistant who respond to user query on the basis of available context.
+Return the output as JSON object.
+
+context:
+{context}
+"""
+
+messages = [
+    {"role":"user", "content": "What is FS Module ?"},
+    {"role": "system", "content": system_prompt}
+]
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    response_format={"type":"json_object"},
+    messages=messages
+)
+
+print("Output : ")
+print(response.choices[0].message.content)
